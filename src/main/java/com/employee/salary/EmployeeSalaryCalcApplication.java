9968188cbc18099375f3c4523647921e3a505374
kman66 <karol.manterys@gmail.com>
2018-10-29 18:09:31 +0100

    first commit

---
+++ src/main/java/com/employee/salary/EmployeeSalaryCalcApplication.java
@@ -0,0 +1,12 @@
+package com.employee.salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSalaryCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSalaryCalcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/employee/salary/EmployeeSalaryCalcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.employee.salary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeSalaryCalcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
